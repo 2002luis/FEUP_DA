5f28771972eee6b5d356c20082ebb7b9a97ec397
2002luis <luis.m.sousa10@gmail.com>
2023-03-01 13:59:42 +0000

    l

--- TP3/ex5.cpp
+++ TP3/ex5.cpp
@@ -3,10 +3,18 @@
 #include "exercises.h"
 
 #include "MSTTestAux.h"
+#include <map>
 
 std::vector<Vertex *> GreedyGraph::prim() {
-    // TODO
-    return this->vertexSet;
+    for(auto i : this->vertexSet) i->setVisited(true);
+    std::vector<Vertex *> out;
+    out.push_back(this->vertexSet[0]);
+    this->vertexSet[0]->setVisited(true);
+    std::map<Vertex,std::pair<Vertex,int>> minDist;
+    for(auto i : this->vertexSet[0]->getAdj()){
+        minDist.insert({i->getDest(),{this->vertexSet[0],i->getWeight()}});
+    }
+    return out;
 }
 
 /// TESTS ///
